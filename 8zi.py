bc9335d65ffbb247cc20a3a7946591d161d6f90e
Wu Xuening <deep_med@163.com>
2026-05-28 06:40:37 +0800

    fix liunian year pillar: use June 1 to avoid pre-lichun offset

--- 8zi.py
+++ 8zi.py
@@ -79,7 +79,7 @@ def calculate_bazi(data: BaziRequest):
             for i in range(5):
                 y = current_year + i
                 try:
-                    lunar_y = Solar(y, 1, 1, 12, 0, 0).getLunar()
+                    lunar_y = Solar(y, 6, 1, 12, 0, 0).getLunar()
                     ec_y = lunar_y.getEightChar()
                     liunian_list.append({
                         "year": y,
